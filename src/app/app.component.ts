b0bedad8b41c1772e70b16d4b9235cf7ef27df37
Isaia Maccali <isaiamaccali@gmail.com>
2025-08-18 11:29:11 +0200

    first build and deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ButtonComponent } from '../components/design-system/atoms/button/button/button.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   standalone: true,
